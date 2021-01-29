1ad823c07900377b58f390153d2bfc580378191e
Nicolas <cisternas@et.esiea.fr>
2021-01-29 22:35:41 +0100

    :sparkles:float are changed to double, to increase precision Creation of the Sphere class inheriting from PrimitiveObject Addition of the intersectionSphere() function that return a vector of intersection points.

--- BlasterProject/PrimitiveObjects/Sphere.cpp
+++ BlasterProject/PrimitiveObjects/Sphere.cpp
@@ -11,11 +11,33 @@ Sphere::Sphere(Vector3 pCenter, double pRadius)
     center = pCenter;
     radius = pRadius;
 }
-
+// https://ibb.co/TY0W1n2
+// According to the schema, L will be the direction Vector going from the origin of the ray, and going to the center of the sphere.
+// tc stands for "t-cut"
 std::vector<Vector3> Sphere::intersect(Ray pRay)
 {
     std::vector<Vector3> result;
-    // Add here the math to find the intersect between the sphere (this) and pRay.
+    double radius = gRadius(), d, tc, t1c;
+    Vector3 L = gCenter() - pRay.gOrigin();   
+
+    tc = L * pRay.gDirection();
+
+    if (tc < 0)
+    {
+        std::cout << "\nNo intersection";
+        return result;
+    }
+
+    d = sqrt(L * L - tc * tc);
+    if (d > radius)
+    {
+        std::cout << "\nNo intersection";
+        return result;
+    }
+    t1c = sqrt(radius * radius - d * d);
+
+    result.push_back(pRay.point(tc - t1c));
+    result.push_back(pRay.point(tc + t1c));
 
     return result;
 }
