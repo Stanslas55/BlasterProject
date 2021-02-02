29d6efe1a4978d6fe9c83f48ebd4b997193de91e
Nicolas <cisternas@et.esiea.fr>
2021-02-02 03:10:18 +0100

    ⚡️ Improved intersection Polygon Can now detect intersection in every plane

--- BlasterProject/PrimitiveObjects/Polygon.cpp
+++ BlasterProject/PrimitiveObjects/Polygon.cpp
@@ -33,21 +33,25 @@ std::vector<Vector3> Polygon::intersect(const Ray& pRay) const{
         double* verty = new double[m_nbCorners];
 
         double pX = point[0].x();
-        double pY = point[0].z();
+        double pY = point[0].y();        
+        
+        double pZ = point[0].z();
 
-        int index = 0, inter = 0, i, j;
+        int index = 0, inter = 0, inter2 = 0, i, j;
 
         for(index = 0; index < m_nbCorners; index++) {
             vertx[index] = m_corners[index].x();
-            verty[index] = m_corners[index].z();
+            verty[index] = m_corners[index].y();
         }                              
         for(i = 0, j = m_nbCorners - 1; i < m_nbCorners; j = i++) {
             if( ((verty[i] > pY) != (verty[j] > pY)) && (pX < (vertx[j] - vertx[i]) * (pY - verty[i]) / (verty[j] - verty[i]) + vertx[i]) )
                 inter = !inter;
+            if (((verty[i] > pZ) != (verty[j] > pZ)) && (pX < (vertx[j] - vertx[i]) * (pZ - verty[i]) / (verty[j] - verty[i]) + vertx[i]))
+                inter2 = !inter2;
         }
         delete[] vertx;
         delete[] verty;
-        return (inter % 2 == 0) ? std::vector<Vector3>() : point; // If inter is even, it's not in the polygon.               
+        return (inter % 2 == 0 && inter2 % 2 == 0) ? std::vector<Vector3>() : point; // If a both are even, it's not in the polygon.               
     }
     std::cout << "No intersection between the quadri and the ray." << std::endl;
     return std::vector<Vector3>();
